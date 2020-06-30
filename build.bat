e959aeb7089feb0dc190555cf7fb094a4e3ac6ab
wessel mast <mastwessel@gmail.com>
2020-06-30 14:40:20 +0200

    removed caching and debug transpose code

--- build.bat
+++ build.bat
@@ -9,16 +9,18 @@ set linkerpaths=-LLib\\10.0.17763.0\\um\\x86\\
 set includepaths=-IInclude\\10.0.17763.0\\
 set libraries=-lopengl32 -luser32 -lgdi32 -ld3d11
 set compiler=clang++
-set caching=ccache
+set caching=
 
-WHERE %caching%
-IF %ERRORLEVEL% NEQ 0 (
-   echo %caching% not found, removing..
-   set caching=
+IF NOT [%caching%] == [] (
+   WHERE %caching%
+   IF %ERRORLEVEL% NEQ 0 (
+      echo %caching% not found, removing..
+      set caching=
+   )
 )
 
-IF "%1" == "" set config=debug
-IF NOT "%~2"== "" for /f "tokens=1,* delims= " %%a in ("%*") do set compiler=%%b
+IF [%1] == [] set config=debug
+IF NOT [%~2] == [] for /f "tokens=1,* delims= " %%a in ("%*") do set compiler=%%b
 
 echo %compiler%
 
@@ -28,8 +30,8 @@ echo Building %config%...
 IF NOT EXIST .\bin\%config%\ mkdir .\bin\%config%\
 robocopy .\res .\bin\%config%\res /E /NFL /NDL /NJH /NJS
 
-IF "%config%" == "debug" GOTO Debug
-IF "%config%" == "release" GOTO Release
+IF [%config%] == [debug] GOTO Debug
+IF [%config%] == [release] GOTO Release
 echo. && echo ERROR: %config% is not a valid build configuration && echo.
 rmdir .\bin\%config%\
 GOTO :eof
